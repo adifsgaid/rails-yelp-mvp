a336a907e78ad0ca5b8c09882b98854923c6e7ac
adif sgaid <infosgaid@gmail.com>
2020-11-11 15:36:53 +0100

    yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+restaurant1 = Restaurant.create!(name: "yemma", adress: "paris", category: "japanese" )
+restaurant2 = Restaurant.create!(name: "trattoria", adress: "toulouse", category: "chinese" )
+restaurant3 = Restaurant.create!(name: "mcdo", adress: "bordeaux", category: "french" )
+restaurant4 = Restaurant.create!(name: "pizza hut", adress: "lyon", category: "belgian" )
+restaurant5 = Restaurant.create!(name: "chez mama", adress: "lyon", category: "italian" )
